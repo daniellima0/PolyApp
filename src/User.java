4a84e898619886f665283053c225fb36ec63348f
Daniel Lima <danielmlimag@gmail.com>
2024-11-15 11:29:49 +0100

    feat: unify userFactory and User and delete unecessary methods

--- src/User.java
+++ src/User.java
@@ -1,9 +1,65 @@
-public interface User {
-    String getNom();
-    String getPrenom();
-    String getMail();
+import java.util.ArrayList;
+import java.util.List;
 
-    String getInfoProfil();
-    
-    // Other common methods can be added here if needed.
+abstract class User {
+    private String nom;
+    private String prenom;
+    private String mail;
+    private List<Message> messagesRecus;
+    private List<Post> wishList;
+
+    public User(String nom, String prenom, String mail) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.messagesRecus = new ArrayList<>();
+        this.wishList = new ArrayList<>();
+    }
+
+    public void getInfoProfil() {
+        System.out.println("Nom:" + nom);
+        System.out.println("Prenom:" + prenom);
+        System.out.println("Mail:" + mail);
+    }
+
+    // Getters and Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public List<Message> getMessagesRecus() {
+        return messagesRecus;
+    }
+
+    public List<Post> getWishList() {
+        return wishList;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setMessagesRecus(List<Message> messagesRecus) {
+        this.messagesRecus = messagesRecus;
+    }
+
+    public void setPostsAimees(List<Post> wishList) {
+        this.wishList = wishList;
+    }
 }
