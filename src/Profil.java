0d80c97bd2561c0328e86cad73121ba2bf6a4986
Axelle <160240449+aquellery@users.noreply.github.com>
2024-11-28 15:20:37 +0100

    affichage du profil

--- src/Profil.java
+++ src/Profil.java
@@ -6,8 +6,20 @@ public class Profil {
     }
     public void profilApp(){
         System.out.println("Vous vous trouvez dans le profil");
-        System.out.println("L'utilisateur est : " + u.getPrenom()+" " +u.getNom());
-        System.out.println("Son adresse mail " + u.getMail());
+        if (u.getType().equals("Etudiant") || u.getType().equals("Delegue")){
+            System.out.println("Profil étudiant");
+            u.getInfoProfil();
+        }
+
+        if (u.getType().equals("BDE")){
+            System.out.println("Profil BDE");
+            u.getInfoProfil();
+        }
+
+        if (u.getType().equals("Admin")){
+            System.out.println("Profil Administrateur");
+            u.getInfoProfil();
+        }
     }
     
 }
