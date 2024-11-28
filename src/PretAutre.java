f3bc91f83c46071dfcc4c9e3b21200c86a3bf90e
Axelle <160240449+aquellery@users.noreply.github.com>
2024-11-28 13:52:11 +0100

    correction PretAutre

--- src/PretAutre.java
+++ src/PretAutre.java
@@ -1,10 +1,24 @@
-public class PretAutre extends Post {
+public class PretAutre extends PretMaterielPost {
     private String photo;
     private String etatInitial;
+    private String etatFinal;
+
     public PretAutre(String titre, String description, String photo, String etat_initial ){
-        super(titre, description);
+        super(titre, description, photo, etat_initial);
         this.photo=photo;
         this.etatInitial=etat_initial;
+    }
+
+    public String getPhoto(){
+        return this.photo;
+    }
+
+    public String getEtatInitial(){
+        return this.etatInitial;
+    }
+
+    public void setEtatFinal(String etat){
+        this.etatFinal=etat;
     }
 
 }
