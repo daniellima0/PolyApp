13de0445810154f9c23e2df943198fb2816cc4e4
Andrew Mary Huet de Barochez <andrew.mary-huet-de-barochez@univ-smb.fr>
2024-12-02 15:52:33 +0100

    Added comments

--- src/Post.java
+++ src/Post.java
@@ -1,9 +1,18 @@
+// Le pattern factory me paraît mal compris :
+// - Il faut normalement dissocier les objets (Posts, Users...) des classes créatices
+// - donc créer une interface PostFactory et UserFactory ainsi que leur descendants concrets
+//   contenants les méthodes constructrices
+//
+// voir https://moodle.univ-smb.fr/pluginfile.php/111840/mod_resource/content/1/INFO732_CM2_DesignPatterns_2020.pdf pages 25-31
+//
+// La hiérarchisation des Posts et des Users en elle même est très bien.
 
 abstract class Post {
     private String titre;
     private String description;
     private String lien; // Unique identifier for the post
 
+    // Bien !
     private static int counter = 1; // Static counter to track the number of posts created
 
     public Post(String titre, String description) {
